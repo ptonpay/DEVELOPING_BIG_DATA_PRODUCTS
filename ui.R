97665b17394d66e72e31fd3a44c178ba9878c7d1
ptonpay <pranay_tonpay@yahoo.com>
2014-12-04 14:08:14 -0800

    changes made to ui.R

--- ui.R
+++ ui.R
@@ -4,9 +4,9 @@ shinyUI(pageWithSidebar(
 
   sidebarPanel(
       h5('Dataset used   - iris.'),
-      h6('This famous iris data set gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are Iris setosa, versicolor, and virginica.'),
       h5('Algorithm used - RandomForest'),
-      h6('Based on the values of Sepal Length, Sepal Width, Petal Length and Petal Width the model would predict whether the species is one of setosa, versicolor or virginica' ),
+      h5('Enter values for the below fields and click on Perdict'),
+      h5('On the mail panel, you would see the result of the predicted species'),
       numericInput('sl', "Sepal Length", value=0, min = 0, max = 200, step=0.1),
       numericInput('sw', "Sepal Width", value=0 , min = 0, max = 200, step=0.1),
       numericInput('pl', "Petal Length", value=0 , min = 0, max = 200, step=0.1),
@@ -14,6 +14,10 @@ shinyUI(pageWithSidebar(
       submitButton("Predict")
   ),
   mainPanel(
+      h6('This famous iris data set gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are Iris setosa, versicolor, and virginica.'),
+      h6('Based on the values of Sepal Length, Sepal Width, Petal Length and Petal Width the model would predict whether the species is one of setosa, versicolor or virginica' ),
+      p(''),
+      p(''),
       p('Predicted Species'),
       textOutput("species")
   )
